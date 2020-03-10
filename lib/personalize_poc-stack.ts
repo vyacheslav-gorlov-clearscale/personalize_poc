c63927d219fae8ca86bec01b97a51fda150eeb04
gorlov-clearscale <vyacheslav.gorlov@clearscale.net>
2020-03-10 22:31:16 +0500

    - init

--- lib/personalize_poc-stack.ts
+++ lib/personalize_poc-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import { CfnNotebookInstance } from '@aws-cdk/aws-sagemaker';
+import { Role, ManagedPolicy, ServicePrincipal } from "@aws-cdk/aws-iam";
 
 export class PersonalizePocStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const iamRole = new Role(this, "NotebookInstanceRole", {
+      assumedBy: new ServicePrincipal("sagemaker.amazonaws.com"),
+      path: "/",
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonSageMakerFullAccess"),
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+        ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonPersonalizeFullAccess"),
+        ManagedPolicy.fromAwsManagedPolicyName("IAMFullAccess")
+      ]
+    });
+
+    // noinspection JSUnusedLocalSymbols
+    const sageMakerInstance = new CfnNotebookInstance(this, "NotebookInstance", {
+      instanceType: "ml.t3.medium",
+      roleArn: iamRole.roleArn,
+      notebookInstanceName: "Forecast-POC",
+      defaultCodeRepository: "https://github.com/gorlov-clearscale/personalize_poc.git",
+      volumeSizeInGb: 20
+    });
   }
 }
